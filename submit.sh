08915d0f21a7a43aededeb3d0b54fa4307b66956
Peter Marquardt <wwwutz@gmail.com>
2014-06-13 09:14:06 +0200

    applied -B magic

--- submit.sh
+++ submit.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
-cp /home/wwwutz/2048/map2k /scratch/cluster/wwwutz/
-NUM=2000
-rm /scratch/cluster/wwwutz/job.stdout.[0-9]*
-JC=$((1+`cat /scratch/cluster/wwwutz.jobcnt`))
-echo ${JC}>/scratch/cluster/wwwutz.jobcnt
-for i in `seq -w 1 ${NUM}`; do
- JF=/scratch/cluster/wwwutz/job.stdout.$i
- mxq_submit --stdout=${JF} --group-id=sim2k-Randdir-${JC} --threads=1 --memory=20 --time=15 \
- /scratch/cluster/wwwutz/map2k -s 16 -i 10000 -S -Q -R ${i}0000
+BIN=${HOME}/2048
+CLUSTERDISK=/scratch/cluster/wwwutz
+NUM=20
 
+GROUPCOUNTER=${CLUSTERDISK}/.submit.sh.group-id.cnt
+
+cp ${BIN}/map2k ${CLUSTERDISK}/
+ARG=$1
+eval $(${CLUSTERDISK}/map2k -B)
+
+LAST=$((${MAP2K_NUM_STRATS}-1))
+
+STRAT=${ARG:=${LAST}}
+
+NAME=${MAP2K_STRATS_NAME[${STRAT}]}
+
+
+rm ${CLUSTERDISK}/job.stdout.[0-9]*
+JC=$((1+`cat ${GROUPCOUNTER}`))
+echo ${JC}>${GROUPCOUNTER}
+
+GROUPID=${NAME}-${JC}
+for i in `seq -w 1 ${NUM}`; do
+ JF=${CLUSTERDISK}/job.stdout.$i
+ mxq_submit --stdout=${JF} --group-id=${GROUPID} --threads=1 --memory=20 --time=15 \
+ ${CLUSTERDISK}/map2k -s ${STRAT} -i 10000 -S -Q -R ${i}0000
 done
-#cd /scratch/cluster/wwwutz
-#/home/wwwutz/2048/finwait.pl ${NUM};sort -n job.stdout* > /home/wwwutz/2048/stats.Randdir;
+
+echo "waiting for ${NUM} jobs in group \"${GROUPID}\" to finish"
+
+${BIN}/finwait.pl ${NUM}
